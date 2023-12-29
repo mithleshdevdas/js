b7583023ebaab3295c53e9e60b63e70d48dfb64a
mithlesh devdas <mithleshdevdas580@gmail.com>
2023-12-29 15:09:41 +0530

    forder name change

--- varibles.js
+++ varibles.js
@@ -20,5 +20,5 @@ accountPassword = '125dfl#'
 
 accountCity;
 
-console.table([accountId, accountEmail, accountPassword, accountCity])
+console.table([accountId, accountEmail, accountPassword, accountCity]
 
